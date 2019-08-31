0d25472d763457bc8afcef5cf9021ab02136346d
prototux <jason@prototux.net>
2019-08-31 13:49:37 +0200

    first working version for body_score

--- sandbox/body_score.py
+++ sandbox/body_score.py
@@ -1,14 +1,22 @@
 
 # Reverse engineered from amazfit's app (also known as Mi Fit)
+from body_scales import bodyScales
+class bodyScore:
 
-class body_score:
-
-    def __init__(self, age, sex, height, bmi, bodyfat):
+    def __init__(self, age, sex, height, weight, bmi, bodyfat, muscle, water, visceral_fat, bone, basal_metabolism, protein):
         self.age = age
         self.sex = sex
         self.height = height
+        self.weight = weight
         self.bmi = bmi
         self.bodyfat = bodyfat
+        self.muscle = muscle
+        self.water = water
+        self.visceral_fat = visceral_fat
+        self.bone = bone
+        self.basal_metabolism = basal_metabolism
+        self.protein = protein
+        self.scales = bodyScales(age, height, sex, weight)
 
     def getBodyScore(self):
         score = 100
@@ -20,30 +28,21 @@ class body_score:
         score -= self.getBoneDeductScore()
         score -= self.getBasalMetabolismDeductScore()
         score -= self.getProteinDeductScore()
-
         return score
 
-    def getBodyScoreScale(self):
-        # very bad, bad, normal, good, better
-        return [50.0, 60.0, 80.0, 90.0]
-
-    def calculate(data, a, b, c, d):
+    def calculate(self, data, a, b, c, d):
         result = ((data - b) / (a - b)) * float(c - d)
         if result >= 0.0:
             return result
         return 0.0
 
-    def getBmiDeductScore():
+    def getBmiDeductScore(self):
         if not self.height >= 90:
             # "BMI is not reasonable
             return 0.0
 
         arr = [14.0, 15.0]
-        if self.bmi >= arr[1]:
-            if self.bmi >= 18.5 or self.age < 18:
-                return value
-            else:
-                return self.calculate(self.bmi, arr[1], 18.5, 15, 5) + 5.0
+        normalfat = self.scales.getFatPercentageScale()
 
         # Perfect range (bmi >= 18.5 and bodyfat not high for adults, bmi >= 15.0 for kids
         if self.bmi >= 18.5 and self.age >= 18 and self.bodyfat < normalfat[2]:
@@ -70,32 +69,8 @@ class body_score:
                 else:
                     return 0.0
 
-    def getBodyFatDeductScore():
-        normal = []
-        if sex == 'male':
-            if age < 18:
-                normal = [7.0, 16.0, 25.0, 30.0]
-            elif age < 40:
-                normal = [11.0, 17.0, 22.0, 27.0]
-            elif age < 60:
-                normal = [12.0, 18.0, 23.0, 28.0]
-            else:
-                normal = [14.0, 20.0, 25.0, 30.0]
-        elif sex == 'female':
-            if age < 12:
-                normal = [12.0, 21.0, 30.0, 34.0]
-            elif age < 14:
-                normal = [15.0, 24.0, 33.0, 37.0]
-            elif age < 16:
-                normal = [18.0, 27.0, 36.0, 40.0]
-            elif age < 18:
-                normal = [20.0, 28.0, 37.0, 41.0]
-            elif age < 40:
-                normal = [21.0, 28.0, 35.0, 40.0]
-            elif age < 60:
-                normal = [22.0, 29.0, 36.0, 41.0]
-            else:
-                normal = [23.0, 30.0, 37.0, 42.0]
+    def getBodyFatDeductScore(self):
+        normal = self.scales.getFatPercentageScale()
 
         best = 0.0
         if self.sex == 'male':
@@ -121,22 +96,8 @@ class body_score:
                 return self.calculate(self.bodyfat, 1.0, normal[0], 3, 10) + 3.0
 
 
-    def getMuscleDeductScore():
-        normal = []
-        if self.sex == 'male':
-            if self.height < 160:
-                normal = [38.5, 46.6]
-            elif self.height < 170:
-                normal = [44.0, 52.5]
-            else:
-                normal = [49.4, 59.5]
-        elif self.sex == 'female':
-            if self.height < 150:
-                normal = [29.1, 34.8]
-            elif self.height < 160:
-                normal = [32.9, 37.6]
-            else:
-                normal = [36.5, 42.6]
+    def getMuscleDeductScore(self):
+        normal = self.scales.getMuscleMassScale()
 
         # For some reason, there's code to return self.calculate(muscle, normal[0], normal[0]+2.0, 3, 5) + 3.0
         # if your muscle is between normal[0] and normal[0] + 2.0, but it's overwritten with 0.0 before return
@@ -149,12 +110,8 @@ class body_score:
 
     # No malus = normal or good; maximum malus (10.0) = less than normal-5.0;
     # malus = between 5 and 10, on your water being between normal-5.0 and normal
-    def getWaterDeductScore():
-        normal = []
-        if self.sex == 'male':
-            normal = [55.0, 65.1]
-        elif self.sex == 'female':
-            normal = [45.0, 60.1]
+    def getWaterDeductScore(self):
+        normal = self.scales.getWaterPercentageScale()
 
         if self.water >= normal[0]:
             return 0.0
@@ -165,8 +122,9 @@ class body_score:
 
     # No malus = normal; maximum malus (15.0) = very high; malus = between 10 and 15
     # with your visceral fat in your high range
-    def getVisceralFatDeductScore():
-        normal = [10.0, 15.0]
+    def getVisceralFatDeductScore(self):
+        normal = self.scales.getVisceralFatScale()
+
         if self.visceral_fat < normal[0]:
             # For some reason, the original app would try to
             # return 3.0 if vfat == 8 and 5.0 if vfat == 9
@@ -177,7 +135,7 @@ class body_score:
         else:
             return self.calculate(self.visceral_fat, normal[1], normal[0], 15, 10) + 10.0
 
-    def getBoneDeductScore():
+    def getBoneDeductScore(self):
         normal = []
         if self.sex == 'male':
             if self.weight < 60.0:
@@ -194,30 +152,16 @@ class body_score:
             else:
                 normal: [1.8, 3.9]
 
-        if self.bone_mass >= normal[0]:
+        if self.bone >= normal[0]:
             return 0.0
-        elif self.bone_mass <= (normal[0] - 0.3):
+        elif self.bone <= (normal[0] - 0.3):
             return 10.0
         else:
-            return self.calculate(self.bone_mass, normal[0] - 0.3, normal[0], 10, 5) + 5.0
+            return self.calculate(self.bone, normal[0] - 0.3, normal[0], 10, 5) + 5.0
 
-    def getBasalMetabolismDeductScore():
+    def getBasalMetabolismDeductScore(self):
         # Get normal BMR
-        normal = 0
-        if sex == 'male':
-            if age <= 29:
-                normal = 21.6 * self.weight
-            elif age <= 49:
-                normal = 20.07 * self.weight
-            else:
-                normal = 19.35 * self.weight
-        elif sex == 'female':
-            if age <= 29:
-                normal = 21.24 * self.weight
-            elif age <= 49:
-                normal = 19.53 * self.weight
-            else:
-                normal = 18.63 * self.weight
+        normal = self.scales.getBMRScale()[0]
 
         if self.basal_metabolism >= normal:
             return 0.0
@@ -229,7 +173,7 @@ class body_score:
 
 
     # Get protein percentage malus
-    def getProteinDeductScore():
+    def getProteinDeductScore(self):
         # low: 10,16; normal: 16,17
         # Check age or return 0
 
